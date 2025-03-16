a2f226a276507d67eb9216d5310fd6dd563ec87d
Angel Daniel Aviles Cruz <aviles@iMac-de-Angel.local>
2025-03-16 02:58:57 -0600

    implementación básica de provider - Cambio de estaod en el widget

--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+import 'package:yes_no_app/main.dart';
+import 'package:yes_no_app/presentation/providers/chat_provider.dart';
 import 'package:yes_no_app/presentation/widgets/chat/his_message_bubble.dart';
 import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
 import 'package:yes_no_app/presentation/widgets/shared/message_fielg_box.dart';
@@ -28,17 +32,28 @@ class ChatScreen extends StatelessWidget {
 class _ChatView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final chatProvider = context.watch<ChatProvider>();
+
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10),
         child: Column(
           children: [
-            Expanded(child: ListView.builder(itemBuilder: (context, index) {
-              //Ternario que diferencia pares de impares
-              return (index % 2 == 0)
-                  ? const HisMessageBubble()
-                  : const MyMessageBubble();
-            })),
+            Expanded(
+                child: ListView.builder(
+                    itemCount: chatProvider
+                        .messageList.length, //límite de filas a mostrar
+                    itemBuilder: (context, index) {
+                      //Ternario que diferencia pares de impares
+                      // return (index % 2 == 0)
+                      //     ? const HisMessageBubble()
+                      //     : const MyMessageBubble();
+                      final message = chatProvider.messageList[index];
+
+                      return (message.fromWho == FromWho.his)
+                          ? const HisMessageBubble()
+                          : MyMessageBubble(message: message);
+                    })),
             const MessageFielgBox()
           ],
         ),
